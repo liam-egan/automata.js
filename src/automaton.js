254a9e5d4e18c4fd34eb9269ea90cb3af9e78ca0
Liam <peculiarexpression@gmail.com>
2018-08-05 15:49:06 -0400

    Update docs

--- src/automaton.js
+++ src/automaton.js
@@ -17,14 +17,12 @@ import Cell from './cell'
 /**
  * A function which takes the current state of the automaton and produces the
  * updated state
- * @function
  * @typedef NextFunction
  * @return {Automaton} An automaton with the updated state
  */
 
 /**
  * A function which produces the next state of a given cell
- * @function
  * @typedef NextCellFunction
  * @param {number} x The x coord of the cell
  * @param {number} y The y coord of the cell
