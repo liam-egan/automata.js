ffdc4bcff332826223be0cab0385c50581915d86
Liam <16628912+liam-egan@users.noreply.github.com>
2018-12-25 20:42:06 -0500

    Remove unnecessary tests and add more tests

--- test/automaton.spec.js
+++ test/automaton.spec.js
@@ -1,45 +1,76 @@
 import { expect } from 'chai'
 import Automaton from '../src/automaton'
+import Cell from '../src/cell'
 
 describe('Automaton', () => {
-  it('returns an object', () => {
-    const actual = new Automaton({
-      produceNextCellState: () => false,
-    })
-
-    expect(actual).to.be.an('object')
-  })
-
-  it('returns an object with all necessary fields', () => {
-    const actual = new Automaton({
-      produceNextCellState: () => false,
-    })
-
-    expect(actual).to.have.property('getNextState')
-    expect(actual).to.have.property('cells')
-  })
-
-  it('returns an object with fields of the right type', () => {
-    const actual = new Automaton({
-      produceNextCellState: () => false,
-    })
-
-    expect(actual.getNextState).to.be.a('function')
-    expect(Array.isArray(actual.cells)).to.eq(true)
-  })
-
-  it('throws a TypeError when not enough fields are present', () => {
+  it('throws a TypeError when produceCellState is not present', () => {
     expect(() => new Automaton()).to.throw(TypeError)
+  })
+
+  it('throws a TypeError when produceCellState is not a function', () => {
+    expect(() => {
+      new Automaton({
+        produceNextCellState: false,
+      }).to.throw(TypeError)
+    })
+  })
+
+  it('throws a RangeError when width is 0', () => {
+    expect(() => {
+      new Automaton({
+        produceNextCellState: () => new Cell(false),
+        width: 0,
+      })
+    }).to.throw(RangeError)
+  })
+
+  it('throws a RangeError when height is 0', () => {
+    expect(() => {
+      new Automaton({
+        produceNextCellState: () => new Cell(false),
+        height: 0,
+      })
+    }).to.throw(RangeError)
+  })
+
+  it('throws a RangeError both width and height are 0', () => {
+    expect(() => {
+      new Automaton({
+        produceNextCellState: () => new Cell(false),
+        height: 0,
+        width: 0,
+      })
+    }).to.throw(RangeError)
+  })
+
+  it('throws a TypeError when a member of cells is not a Cell', () => {
+    expect(() => {
+      new Automaton({
+        produceNextCellState: () => new Cell(false),
+        cells: [1],
+      })
+    })
   })
 
   it('creates cells with the passed in width and height', () => {
     const actual = new Automaton({
-      produceNextCellState: () => false,
+      produceNextCellState: () => new Cell(false),
       width: 10,
       height: 15,
     })
 
     expect(actual.cells.length).to.eq(15)
     expect(actual.cells[0].length).to.eq(10)
+  })
+
+  describe('getNextState method', () => {
+    it('should return an Automaton', () => {
+      const automaton = new Automaton({
+        produceNextCellState: () => new Cell(false),
+      })
+      const actual = automaton.getNextState()
+
+      expect(actual).to.be.an.instanceof(Automaton)
+    })
   })
 })
--- test/cell.spec.js
+++ test/cell.spec.js
@@ -2,22 +2,12 @@ import { expect } from 'chai'
 import Cell from '../src/cell.js'
 
 describe('Cell', () => {
-  it('should return an object', () => {
-    const actual = new Cell()
-    expect(actual).to.be.an('object')
-  })
-
-  it('should return an object with an "enabled" field', () => {
-    const actual = new Cell()
-    expect(actual).to.have.property('enabled')
-  })
-
-  it('should return an enabled Cell when passed a boolean', () => {
+  it('returns an enabled Cell when passed a boolean', () => {
     const actual = new Cell(true)
     expect(actual.enabled).to.eq(true)
   })
 
-  it('should return an enabled Cell when passed an options object', () => {
+  it('returns an enabled Cell when passed an options object', () => {
     const actual = new Cell({
       enabled: true,
     })
